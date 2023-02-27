acc8a567acf7f182bdac2e25074d3149f7752e15
Alexey Sokolov <alexvyber@gmail.com>
2023-02-27 09:23:08 +0300

    new stuff

---
+++ working/src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      asdf
+      {/* <Nav /> */}
+      <br />
+      <br />
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/useReducer" element={<UseReducer />} /> */}
+        {/* <Route path="/useMemo" element={<UseMemo />} /> */}
+        {/* <Route path="/useCallback" element={<UseCallback />} /> */}
+        {/* <Route path="/useRef" element={<UseRef />} /> */}
+        {/* <Route path="/useLayoutEffect" element={<UseLayoutEffect />} /> */}
+        {/* <Route path="/useId" element={<UseId />} /> */}
+      </Routes>
+    </div>
+  )
+}
+
+export default App
